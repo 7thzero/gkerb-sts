2fcf314d9346efda9c8134b2cdfcf5a28794e1ce
7thZero <zero@7thzero.com>
2020-07-28 13:31:48 -0600

    Notes/example script to deploy

---
+++ scripts/deploy-local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Deploys gkerb-sts locally
+#
+# Be sure to add a bash alias if you want it to replace 'kerb-sts'!
+#
+# Alias example:
+#   $ alias kerb-sts='gkerb-sts -IdentityIrp=<identity_irp_url_here>'
+
+rm -rf /home/$USER/go/bin/gkerb-sts
+cp -n gkerb-sts /home/$USER/go/bin/gkerb-sts
+
+rm -rf /home/$USER/.local/bin/gkerb-sts
+cp -n gkerb-sts /home/$USER/.local/bin/gkerb-sts
